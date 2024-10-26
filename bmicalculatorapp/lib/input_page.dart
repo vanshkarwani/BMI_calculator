ee3a4f629ab38c4f5856c21e8838174258e888da
vanshkarwani <you@example.com>
2024-10-26 21:00:41 +0530

    commit

--- bmicalculatorapp/lib/input_page.dart
+++ bmicalculatorapp/lib/input_page.dart
@@ -8,6 +8,7 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  static const bottomcontainerheight = 80.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,12 +19,66 @@ class _InputPageState extends State<InputPage> {
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         )),
       ),
-      body: const Center(
-        child: Text(
-          "body text",
-          style: TextStyle(color: Colors.white),
-        ),
+      body: Column(
+        children: [
+          // first row two cards
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(const Color(0xFF1D1F33)),
+                ),
+                Expanded(
+                  child: ReusableCard(const Color(0xFF1D1F33)),
+                )
+              ],
+            ),
+          ),
+          // single card
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(const Color(0xFF1D1F33)),
+                )
+              ],
+            ),
+          ),
+          // two cards
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(const Color(0xFF1D1F33)),
+                ),
+                Expanded(
+                  child: ReusableCard(const Color(0xFF1D1F33)),
+                )
+              ],
+            ),
+          ),
+
+          // container like
+          Container(
+            height: bottomcontainerheight,
+            color: Color(0xFFEB1555),
+          )
+        ],
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard(this.color);
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
